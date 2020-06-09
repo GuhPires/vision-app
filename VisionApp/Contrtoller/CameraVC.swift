fe49df9572f847c91b4da63438b27b7cc1f47269
Gustavo <gustavo.goncalvespires@gmail.com>
2020-06-09 12:18:00 -0300

    Project setup

---
+++ VisionApp/Contrtoller/CameraVC.swift
@@ -0,0 +1,20 @@
+//
+//  CameraVC.swift
+//  VisionApp
+//
+//  Created by Gustavo Mac Mini on 09/06/20.
+//  Copyright © 2020 DEVX. All rights reserved.
+//
+
+import UIKit
+
+class CameraVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
